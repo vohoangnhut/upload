e94ad5d6436b43fa19edadcc3d0162a0a7a214cd
Nhut.Vo <nhut.vo@dou.com.vn>
2017-07-11 10:47:21 +0700

    init

--- app.js
+++ app.js
@@ -13,14 +13,14 @@ app.get('/', (req,res)=>{
     res.end('still alive')
 })
 
-app.get('/upload',allowCrossDomain() , uploadAPI.upload)
-
-app.listen(app.get('port'), function(){console.log(`app is running on port ${app.get('port')}`)})
-
+app.get('/upload',
 function allowCrossDomain (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
     next();
-}
+} , uploadAPI.upload)
+
+app.listen(app.get('port'), function(){console.log(`app is running on port ${app.get('port')}`)})
+
