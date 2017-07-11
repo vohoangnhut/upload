028dc6a2db8b1cf9081ec4054a964a5eeda60237
Nhut.Vo <nhut.vo@dou.com.vn>
2017-07-11 10:17:57 +0700

    init

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const path =  require('path')
 const app = express();
 
+app.use(express.static(path.join(__dirname,'upload'),{maxAge: 0}))//315360000 }))
+
 app.set('port', process.env.PORT || 8080)
 
 app.get('/', (req,res)=>{
