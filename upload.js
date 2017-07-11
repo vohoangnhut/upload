85e707ae253564bcd5faf8ee52d5d6e0d55962ee
Nhut.Vo <nhut.vo@dou.com.vn>
2017-07-11 13:23:45 +0700

    init

--- upload.js
+++ upload.js
@@ -9,7 +9,9 @@ function upload(req,res){
 
     var namefile = '_' + Math.random().toString(36).substr(2, 9) + '.png';
 
-    fs.writeFile( path.join(__dirname,'public/upload/') + namefile , req.body.file , 'base64', function(err) {
+    console.log('__dirname : '+ __dirname)
+
+    fs.writeFile( path.join(__dirname,'../public/upload/') + namefile , req.body.file , 'base64', function(err) {
         if (err) {
             //response.send("failed to save");
             console.log("failed to save : ----" + err)
