64c3c50a654b14282999c5f36699a41d677c827a
Nhut.Vo <nhut.vo@dou.com.vn>
2017-07-11 13:17:50 +0700

    init

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const uploadAPI = require('./upload')
 
 
 
-app.use(express.static(path.join(__dirname,'upload'),{maxAge: 0}))//315360000 }))
+app.use(express.static(path.join(__dirname,'public'),{maxAge: 0}))//315360000 }))
 
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true})) // parse application/x-www-form-urlencoded
 app.use(bodyParser.json({limit: '50mb'})) // parse application/json
--- upload.js
+++ upload.js
@@ -9,7 +9,7 @@ function upload(req,res){
 
     var namefile = '_' + Math.random().toString(36).substr(2, 9) + '.png';
 
-    fs.writeFile( __dirname + namefile , req.body.file , 'base64', function(err) {
+    fs.writeFile( path.join(__dirname,'public/upload/') + namefile , req.body.file , 'base64', function(err) {
         if (err) {
             //response.send("failed to save");
             console.log("failed to save : ----" + err)
